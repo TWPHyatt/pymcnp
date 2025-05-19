1c36cd0e2e73d6b9943a9125354c9af91a2fe703
Thomas Hyatt <thomas.hyatt.2014@live.rhul.ac.uk>
2025-05-19 15:22:38 +0100

    second commit

---
+++ src/pymcnp/__init__.py
@@ -0,0 +1,4 @@
+#import pyg4ometry
+#px = pyg4ometry.mcnp.PX(5)
+
+from . import blockphantom
---
+++ src/pymcnp/blockphantom/block.py
@@ -0,0 +1,12 @@
+
+class Block:
+    def __init__(self, hole):
+        self.hole = hole
+
+
+    def makeBlock(self, hole, block=None, transform = []):
+        return 0
+
+    def makeHaldBlock(self):
+        return 0
+
