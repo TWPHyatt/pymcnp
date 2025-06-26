e7afc012ba56cfe77bb17f01452f4b336885b55f
Thomas Hyatt <thomas.hyatt.2014@live.rhul.ac.uk>
2025-06-26 10:43:59 +0100

    rotationStepsToMatrix func update

--- src/pymcnp/blockphantom/utils.py
+++ src/pymcnp/blockphantom/utils.py
@@ -1,43 +1,44 @@
 import pyg4ometry
 import numpy as _np
 
-def _rotationStepsToMatrix(self, rotation):
+def _rotationStepsToMatrix(self, stepsIn):
     """
     Converts a list of 90-degree step rotations [x, y, z] into a 3x3 rotation matrix
     """
-    if len(rotation) != 3 or not all(isinstance(i, int) for i in rotation):
-        raise TypeError("rotation must be a list of 3 integers [x, y, z] representing 90° steps")
 
-    rotMat = _np.eye(3)
+    steps = _np.array(stepsIn)
 
-    # Rotation around x-axis
-    if rotation[0] != 0:
-        theta = rotation[0] * _np.pi / 2
-        rotX = _np.array([
+    if len(steps) != 3 or not all(isinstance(i, int) for i in steps):
+        raise TypeError("rotation_steps must be a list of 3 integers [x, y, z]")
+
+    def rotX(n):
+        theta = n * _np.pi / 2
+        return _np.array([
             [1, 0, 0],
             [0, _np.cos(theta), -_np.sin(theta)],
             [0, _np.sin(theta), _np.cos(theta)]
         ])
-        rotMat = rotX @ rotMat
 
-    # Rotation around y-axis
-    if rotation[1] != 0:
-        theta = rotation[1] * _np.pi / 2
-        rotY = _np.array([
+    def rotY(n):
+        theta = n * _np.pi / 2
+        return _np.array([
             [_np.cos(theta), 0, _np.sin(theta)],
             [0, 1, 0],
             [-_np.sin(theta), 0, _np.cos(theta)]
         ])
-        rotMat = rotY @ rotMat
 
-    # Rotation around z-axis
-    if rotation[2] != 0:
-        theta = rotation[2] * _np.pi / 2
-        rotZ = _np.array([
+    def rotZ(n):
+        theta = n * _np.pi / 2
+        return _np.array([
             [_np.cos(theta), -_np.sin(theta), 0],
             [_np.sin(theta), _np.cos(theta), 0],
             [0, 0, 1]
         ])
-        rotMat = rotZ @ rotMat
 
-    return rotMat
+    rotationMatrix = _np.eye(3)
+    for axis, step in zip([rotX, rotY, rotZ], steps):
+        if step != 0:
+            rotationMatrix = axis(step) @ rotationMatrix
+
+    return rotationMatrix
+
