a354c3eb3bb49fcfbab6ecbc96466c79bf9f7f28
Thomas Hyatt <thomas.hyatt.2014@live.rhul.ac.uk>
2025-06-26 16:54:41 +0100

    update connector init and add transform func

--- src/pymcnp/blockphantom/connector.py
+++ src/pymcnp/blockphantom/connector.py
@@ -1,15 +1,41 @@
 import pyg4ometry
-
+import numpy as _np
+import utils as _utils
 
 class Connector(pyg4ometry.mcnp.Cell):
-    def __init__(self, translation=[0, 0, 0], rotation=[[1, 0, 0], [0, 1, 0], [0, 0, 1]], length=1.5, cellNumber=None, reg=None):
+    def __init__(self, translation=[0, 0, 0], rotationSteps=[0, 0, 0], length=1.5, cellNumber=None, reg=None):
+
+        rotationMatrix = _utils.rotationStepsToMatrix(rotationSteps)
+        translationVector = _np.array(translation)
 
         surface = pyg4ometry.mcnp.RCC(0, 0, 0, 0, 0, length, 0.3)
 
-        surface_p = surface.transform(rotation=rotation, translation=translation)
+        surface_p = surface.transform(rotation=rotationMatrix, translation=translationVector)
 
         super().__init__(surfaces=surface_p, geometry=surface_p, cellNumber=cellNumber, reg=reg)
 
         if reg:
             m2 = pyg4ometry.mcnp.Material(materialNumber=2, density=2.699)  # aluminium
             reg.addMaterial(m2)
+
+    def transform(self, translation, rotation, isRotationMatrix=False):
+        if isRotationMatrix:
+            rotationMatrix = _np.array(rotation)
+        else:
+            rotationMatrix = _utils.rotationStepsToMatrix(rotation)
+        translationVector = _np.array(translation)
+
+        # new connector (prime)
+        connector_p = Connector(
+            cellNumber=self.cellNumber,
+            reg=self.reg
+        )
+
+        # transform surface
+        surfaces_p = [s.transform(rotation=rotationMatrix.tolist(), translation=translationVector.tolist()) for s in self.surfaceList]
+
+        # update the new connector
+        connector_p.surfaceList = surfaces_p
+        connector_p.geometry = surfaces_p
+
+        return connector_p
