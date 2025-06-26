fa3032f587bd827a96911e66667cf78ce062121a
Thomas Hyatt <thomas.hyatt.2014@live.rhul.ac.uk>
2025-06-26 11:28:19 +0100

    removed computeRotationMatrix func

--- src/pymcnp/blockphantom/block.py
+++ src/pymcnp/blockphantom/block.py
@@ -105,33 +105,6 @@ class Block(pyg4ometry.mcnp.Cell):
 
         return geom
 
-    def _computeRotationMatrix(self, v1, v2):
-        v1 = v1 / _np.linalg.norm(v1)
-        v2 = v2 / _np.linalg.norm(v2)
-
-        crossProd = _np.cross(v1, v2)
-        dotProd = _np.dot(v1, v2)
-
-        if _np.allclose(crossProd, 0):  # vectors are parallel or opposite direction
-            if dotProd > 0:
-                return _np.eye(3)  # no rotation
-            else:
-                return -_np.eye(3)  # opposite direction
-
-        crossProdNorm = _np.linalg.norm(crossProd)
-        crossProd = crossProd / crossProdNorm  # normalise axis
-
-        angle = _np.arccos(dotProd)
-
-        # rodrigues' formula where k is matrix of cross products
-        K = _np.array([
-            [0, -crossProd[2], crossProd[1]],
-            [crossProd[2], 0, -crossProd[0]],
-            [-crossProd[1], crossProd[0], 0]
-        ])
-
-        return _np.eye(3) + _np.sin(angle) * K + (1 - _np.cos(angle)) * _np.dot(K, K)
-
     def transform(self, rotationMatrix, translation):
         rotMat = _np.array(rotationMatrix)
         transMat = _np.array(translation)
